edcd8fdadc8d0e5364c9aa2fe59b69b773550c88
Alexandre Debortoli <debortolidesouzaa@gmail.com>
2024-08-22 18:35:48 -0300

    feat: add favicon and change name to Weather App

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import { Container, Typography } from "@mui/material";
 export const Header: React.FC = () => (
   <Container maxWidth="sm" sx={{ textAlign: "center", mt: 5 }}>
     <Typography variant="h1" fontSize={48} gutterBottom letterSpacing={2}>
-      Amicci Weather
+      Weather App
     </Typography>
   </Container>
 );
